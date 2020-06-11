c638df82d2102a7e7c71c7129a3f8678fbbe1f42
hothotsavage <1725288552@qq.com>
2020-06-11 09:42:10 +0800

    添加文件上传，下载工具类

--- src/main/java/site/marqstree/mung/net/exception/NetException.java
+++ src/main/java/site/marqstree/mung/net/exception/NetException.java
@@ -4,10 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class NetException extends Throwable {
     private Integer code;
     private String message;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
