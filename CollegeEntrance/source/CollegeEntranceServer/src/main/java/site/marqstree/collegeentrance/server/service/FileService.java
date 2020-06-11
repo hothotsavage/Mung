fa44c38460590dd2adc51ec2fbf7cc3cce68e6a8
hothotsavage <1725288552@qq.com>
2020-06-11 10:31:27 +0800

    'init'

--- CollegeEntrance/source/CollegeEntranceServer/src/main/java/site/marqstree/collegeentrance/server/service/FileService.java
+++
@@ -1,9 +0,0 @@
-package site.marqstree.collegeentrance.server.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class FileService {
-
-
-}
--- src/main/java/site/marqstree/mung/net/config/NetConfig.java
+++
@@ -1,6 +0,0 @@
-package site.marqstree.mung.net.config;
-
-//配置主机地址
-public class NetConfig {
-    public static String host="";
-}
